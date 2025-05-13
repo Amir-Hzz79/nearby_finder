a1788e55e8b1e8940182dff4ebeb0297b19827e4
AmirHzz79 <amirhosseinzamani454@gmail.com>
2025-05-13 14:49:41 +0330

    feat(theme): implement theme switching using Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nearby_finder/core/theme/app_theme.dart';
 import 'package:nearby_finder/view/home/home_screen.dart';
 
+import 'core/theme/theme_provider.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: NearbyFinderApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NearbyFinderApp extends ConsumerWidget {
+  const NearbyFinderApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+
     return MaterialApp(
       title: 'Nearby Finder',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: themeMode,
       home: const HomeScreen(),
     );
   }
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../core/theme/theme_provider.dart';
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        appBar: AppBar(title: const Text('Home Page'), centerTitle: true),
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          actions: [
+            IconButton(
+              icon: Icon(
+                ref.watch(themeModeProvider) == ThemeMode.dark
+                    ? Icons.dark_mode
+                    : Icons.light_mode,
+              ),
+              onPressed:
+                  () => ref.read(themeModeProvider.notifier).toggleTheme(),
+            ),
+          ],
+        ),
         body: const SizedBox(),
       ),
     );
