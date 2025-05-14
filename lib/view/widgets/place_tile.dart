ac6e1fd8a7af565d9681a2f6665e6a6fcf6e0677
AmirHzz79 <amirhosseinzamani454@gmail.com>
2025-05-14 10:08:37 +0330

    feat: show place details

--- lib/view/widgets/place_tile.dart
+++ lib/view/widgets/place_tile.dart
@@ -9,11 +9,61 @@ class PlaceTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(place.categoryIconUrl),
-      title: Text(place.name),
-      subtitle: Text(place.categoryName),
-      trailing: Text('${place.distance}m'),
+    Image image = Image.network(place.categoryIconUrl);
+
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          ModalBottomSheetRoute(
+            showDragHandle: true,
+            enableDrag: true,
+            builder:
+                (context) => IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      ListTile(title: Text('Name'), trailing: Text(place.name)),
+                      ListTile(
+                        title: Text('Category'),
+                        trailing: Text(place.categoryName),
+                      ),
+                      ListTile(title: Text('Icon'), trailing: image),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 18),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            /* const SizedBox(width: 10), */
+                            Flexible(
+                              child: Text(
+                                'Address',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Flexible(child: Text(place.address)),
+                          ],
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('Status'),
+                        trailing: Text(place.status),
+                      ),
+                      ListTile(
+                        title: Text('Distance'),
+                        trailing: Text('${place.distance}m'),
+                      ),
+                    ],
+                  ),
+                ),
+            isScrollControlled: true,
+          ),
+        );
+      },
+      child: ListTile(
+        leading: image,
+        title: Text(place.name),
+        subtitle: Text(place.categoryName),
+        trailing: Text('${place.distance}m'),
+      ),
     );
   }
 }
